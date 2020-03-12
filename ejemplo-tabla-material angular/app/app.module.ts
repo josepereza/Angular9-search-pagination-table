f640a5c109e3c7a29ef8265b028bab78fda8037a
josepereza <48152618+josepereza@users.noreply.github.com>
2020-03-12 10:54:40 +0100

    Add files via upload

---
+++ ejemplo-tabla-material angular/app/app.module.ts
@@ -0,0 +1,57 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListadoComponent } from './listado/listado.component';
+import { ProductosService } from "./productos.service";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination'; // <-- import the module
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import { PaginacionComponent } from './paginacion/paginacion.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatSortModule} from '@angular/material/sort';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoComponent,
+    PaginacionComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSortModule,
+    MatIconModule
+  ],
+  providers: [ProductosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ejemplo-tabla-material angular/app/listado/listado.component.ts
@@ -0,0 +1,136 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProductosService } from "../productos.service";
+import {Product} from "../product";
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+  
+  term : string;
+  p: number = 1;
+
+  profileForm: FormGroup;
+  _id = '';
+  prod_name = '';
+  prod_desc = '';
+  prod_price: number = null;
+  actualizar:boolean=false;
+  data: Product[] = [];
+  isLoadingResults = true;
+  
+  displayedColumns: string[] = ['_id', 'prod_name', 'prod_desc', 'prod_price','actions'];
+
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+
+  constructor(private api: ProductosService,private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.profileForm = this.formBuilder.group({
+      
+      'prod_name' : [null, Validators.required],
+      'prod_desc' : [null, Validators.required],
+      'prod_price' : [null, Validators.required]
+    });
+this.Lista(); 
+    
+  }
+  Lista(){
+    this.api.getProducts()
+    .subscribe((res: any) => {
+      this.dataSource= new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+     
+
+      this.data=res;
+      console.log(this.data);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
+    
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  onSubmit() {
+    if (this.actualizar){
+      
+    this.onFormSubmit();
+    return
+    }else {
+    // TODO: Use EventEmitter with form 
+    this.api.addProduct(this.profileForm.value)
+    .subscribe((res: any) => {
+      this.Lista();
+    console.log(res);
+  })
+    }
+}
+editar(id: any) {
+  this.actualizar=true;
+  this.api.getProduct(id).subscribe((data: any) => {
+    this._id = data._id;
+    this.profileForm.setValue({
+      prod_name: data.prod_name,
+      prod_desc: data.prod_desc,
+      prod_price: data.prod_price
+    });
+  });
+}
+
+borrar(id: any) {
+  this.isLoadingResults = true;
+  this.api.deleteProduct(id)
+    .subscribe(res => {
+      this.Lista();
+        this.isLoadingResults = false;
+        
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+    );
+}
+
+onFormSubmit() {
+  this.isLoadingResults = true;
+  this.api.updateProduct(this._id, this.profileForm.value)
+    .subscribe((res: any) => {
+        const id = res._id;
+        this.isLoadingResults = false;
+        this.actualizar=false;
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+    );
+    this.Lista();
+
+}
+cambio(nombre){
+  alert('ha pulsado nombre ' + nombre)
+}
+}
+export interface misDatos {
+  _id: string;
+  prod_name: string;
+  prod_desc:string;
+  prod_price: number;
+}
